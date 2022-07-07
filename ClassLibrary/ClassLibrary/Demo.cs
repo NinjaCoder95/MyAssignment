087b01333f206dcda112a217957d10ad77ff8978
Madhur <madhursrivastava94@gmail.com>
2022-07-07 12:16:20 +0530

    initial commit

---
+++ ClassLibrary/ClassLibrary/Demo.cs
@@ -0,0 +1,19 @@
+﻿namespace ClassLibrary
+{
+
+
+        public class Demo
+        {
+            public void Message()
+            {
+                Console.WriteLine("I am Great");
+
+            }
+        public int counter(string a)
+        {
+            return a.Length;
+        }
+
+        }
+
+}
---
+++ ConsoleAppForClassLibrary/ConsoleAppForClassLibrary/Program.cs
@@ -0,0 +1,11 @@
+﻿using ClassLibrary;
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Demo d = new Demo();
+        d.Message();
+        Console.WriteLine(d.counter("qwertyuiop"));
+    }
+}
